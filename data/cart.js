14280bb9c57dbbb93699666105e372d45d607a50
Daniel Horowitz <daniel.a.horowitz@gmail.com>
2015-01-11 23:25:48 -0800

    Fix up the place
    Add .gitignore, convert to single quotes, style sidebar, add item duplication handler, style add-to-cart buttons, move cart data to simple-storage and overhaul addToCart events

--- data/cart.js
+++ data/cart.js
@@ -1,20 +1,20 @@
-"use strict";
+'use strict';
 
 var itemContainerSelector = 'tbody[id^="item-container"]';
 
 function itemToJson(itemContainer, league) {
     return {
-        "name": $.trim($(itemContainer).find(".item-cell a.title").text()),
-        "data_hash": $(itemContainer).find("span.requirements span.click-button").attr("data-hash"),
-        "thread": $(itemContainer).find("span.requirements span.click-button").attr("data-thread"),
-        "league": league,
+        'name': $.trim($(itemContainer).find('.item-cell a.title').text()),
+        'data_hash': $(itemContainer).find('span.requirements span.click-button').attr('data-hash'),
+        'thread': $(itemContainer).find('span.requirements span.click-button').attr('data-thread'),
+        'league': league,
         //"search_url": window.location.href,
     };
 }
 
 // save league at first so we get the one they actually searched in,
 // in case they change it afterward
-var league = $("form#search select.league").siblings().find("a.chosen-single span").text();
+var league = $('form#search select.league').siblings().find('a.chosen-single span').text();
 
 // append add button to each item
 for (var i = 0; i < $(itemContainerSelector).length; i++) {
@@ -24,6 +24,17 @@ for (var i = 0; i < $(itemContainerSelector).length; i++) {
 };
 
 // emit add signal on button click
-$(".add-to-cart").click(function() {
-    self.port.emit("addToCart", itemToJson($(this).closest(itemContainerSelector), league));
+$('.add-to-cart').click(function() {
+    // store item data
+    self.port.emit('addItem', itemToJson($(this).closest(itemContainerSelector), league));
+
+    // refresh cart
+    self.port.emit('refreshCart');
+
+    $(this).html('Item added to cart!');
+    $(this).fadeOut(2000);
+});
+
+self.port.on('addedDuplicateToCart', function(item) {
+    alert('This item is already in your cart.');
 });
--- data/sidebar.js
+++ data/sidebar.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 function makeItemRow(item) {
     return '<tr class="item-row" data-hash="'
@@ -9,30 +9,49 @@ function makeItemRow(item) {
         + item.league
         //+ '" search-url="'
         //+ item.search_url
-        + '"><td class="item-name">'
+        + '">'
+        + '<td class="item-name">'
         + item.name
         + '</td>'
-        + '<td class="view-item">View</td>'
-        + '<td class="remove-item">X</td>'
-        + '</tr>';
+        + '<td class="view-item"><span class="button">view</span></td>'
+        + '<td class="remove-item"><span class="button">x</span></td>'
+        + '</tr>'
+        + '<tr class="spacer"><td></td></tr>';
 }
 
-addon.port.on("addToCart", function(item) {
-    //console.log(item);
-    //console.log(makeItemRow(item));
-    var newRow = makeItemRow(item)
-    $("#items-table").append(newRow);
-    $(".view-item").off("click").on("click", function() {
-        addon.port.emit(
-            "viewItem",
-            {
-                "league":    $(this).parent().attr("league"),
-                "data_hash": $(this).parent().attr("data-hash"),
-                "thread":    $(this).parent().attr("thread"),
-            }
-        );
-    });
-    $(".remove-item").off("click").on("click", function() {
-        $(this).parent().remove();
-    });
+addon.port.on("refreshCart", function(cart) {
+    $('#items-table').html('');
+
+    for (var i = 0; i < cart.length; i++) {
+        (function (i) {
+            $('#items-table').append(makeItemRow(cart[i]));
+
+            var item_tr = $(
+                'tr[data-hash="'
+                + cart[i].data_hash
+                + '"][thread="'
+                + cart[i].thread
+                + '"][league="'
+                + cart[i].league
+                + '"]'
+            );
+
+            $(item_tr).find('.view-item span').off('click').on('click', function() {
+                addon.port.emit(
+                    'viewItem',
+                    {
+                        'league':    item_tr.attr('league'),
+                        'data_hash': item_tr.attr('data-hash'),
+                        'thread':    item_tr.attr('thread'),
+                    }
+                );
+            });
+
+            $(item_tr).find('.remove-item span').off('click').on('click', function() {
+                $(item_tr).next('tr.spacer').remove();
+                item_tr.remove();
+                addon.port.emit('removeItem', cart[i]);
+            });
+        })(i);
+    }
 });
